b4f51d9ec814fbb10448145d531dc685b219e79f
shwinshaker <justindonchengyu@gmail.com>
2022-01-19 16:12:33 +0000

    new save location

--- run_my.sh
+++ run_my.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # -- Setting --
-gpu=5
+gpu=7
 
 teacher='wrn_40_2'
 # teacher='resnet32x4'
@@ -10,22 +10,26 @@ student='wrn_16_2'
 # student='ShuffleV2' 
 # student='resnet8x4' 
 # method='cifar100_lr_0.05_decay_0.0005'
+# method='cifar100_lr_0.05_decay_0.0005_lip_alpha=1e-05'
+# method='cifar100_lr_0.05_decay_0.0005_omse_alpha=1_linear'
+method='cifar100_lr_0.05_decay_0.0005_lip_alpha=1e-05_omse_alpha=1_linear'
 # method='cifar100_lr_0.05_decay_0.0005_swa=150'
+# method='cifar100_lr_0.05_decay_0.0005_crl_alpha=1_linear_swa=150'
 # method='cifar100_lr_0.05_decay_0.0005_curl_alpha=1_linear_swa=150'
 # method='cifar100_lr_0.05_decay_0.0005_swareg_alpha=1_linear'
-method='cifar100_lr_0.05_decay_0.0005_swareg_alpha=1_linear_swa=150'
+# method='cifar100_lr_0.05_decay_0.0005_swareg_alpha=1_linear_swa=150'
 # method='cifar100_lr_0.05_decay_0.0005_swarank_alpha=1_linear_swa=150'
 trial=0
 
 gamma=1
 alpha=1.0 # 1.0
-beta=1000
+beta=5000
 
 
 # -- Config --
 cur=$PWD
 cd CIFAR-100
-python config_distillation.py --teacher_path "$cur"/experiments/models/"$teacher"_"$method"_trial_"$trial"/"$teacher"_last.pth --model_s "$student" --gpu "$gpu" --beta "$beta" --alpha "$alpha" --gamma "$gamma"
+python config_distillation.py --teacher_path "$cur"/save/models/"$teacher"_"$method"_trial_"$trial"/"$teacher"_last.pth --model_s "$student" --gpu "$gpu" --beta "$beta" --alpha "$alpha" --gamma "$gamma"
 [[ $? -ne 0 ]] && echo 'exit' && exit 2
 
 # -- RUN --
